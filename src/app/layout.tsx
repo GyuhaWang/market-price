5b7229a403d3dd1a73182acba90309b4cf6ab407
GyuhaWang <ethanleast@gmail.com>
2024-07-14 19:16:33 +0900

    Add: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 export const metadata: Metadata = {
 	title: 'MarketPriceVietNam',
 	description: '베트남 현지 물가 공유, 개발자:왕규하, GyuHaWang',
@@ -15,6 +16,7 @@ export default function RootLayout({
 		<html lang="kr">
 			<body>
 				{children}
+				<Analytics />
 				<SpeedInsights />
 			</body>
 		</html>
